9b4217bfd5e6724261303f80a9dc1ec0825fb0d8
SheikhJack <kealebogakido@gmail.com>
2025-07-04 06:21:59 -0700

    prisma

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nachi Learner Management Dashboard",
+  description: "NextGen LMS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
